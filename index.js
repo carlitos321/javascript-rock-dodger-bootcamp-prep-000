47ed4ec41383345ae9b4d46b3f0c83de0b6e9baf
carlitos321 <ca.cabrera321@gmail.com>
2018-02-06 20:57:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ function endGame() {
 }
 
 function moveDodger(e) {
-  document.addEventListener('keydown', function(e){
+  window.addEventListener('keydown', function(e){
     if (e.which !== LEFT_ARROW && e.which !== RIGHT_ARROW) {
       preventDefault();
     } else if (e.which === LEFT_ARROW) {
