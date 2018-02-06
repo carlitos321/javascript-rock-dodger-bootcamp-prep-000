b19c0e58c31de2f58c74507347115c0da7df32d3
carlitos321 <ca.cabrera321@gmail.com>
2018-02-06 21:15:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -132,11 +132,11 @@ function moveDodger(e) {
 }
 
 function moveDodgerLeft() {
+      window.requestAnimationFrame(moveDodgerRight);
       var leftNumbers = DODGER.style.left.replace('px', '')
       var left = parseInt(leftNumbers, 10)
       if (left > 0) {
         DODGER.style.left = `${left - 4}px`
-        window.requestAnimationFrame(moveDodgerLeft);
       }
   // implement me!
   /**
