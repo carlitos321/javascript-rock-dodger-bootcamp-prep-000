d22fb28609e91b7b1f73134ba9f85717648be134
carlitos321 <ca.cabrera321@gmail.com>
2018-02-06 20:51:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ function moveDodgerRight() {
   var rightNumbers = DODGER.style.right.replace('px', '')
   var right = parseInt(rightNumbers, 10)
 
-  DODGER.style.left = `${right + 4}px`
+  DODGER.style.right = `${right + 4}px`
 
   // implement me!
   /**
